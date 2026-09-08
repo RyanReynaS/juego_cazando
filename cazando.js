907df105f2de982a51a2f5dd36382c518485fbef
RyanReynaS <ryanedurs17@gmail.com>
2026-09-08 15:32:44 -0500

    corrrecion variable, integracion graficar rectangulo

--- cazando.js
+++ cazando.js
@@ -1,27 +1,47 @@
+//Obtener canvas y contexto
 let canvas = document.getElementById("areajuego");
 let ctx = canvas.getContext("2d");
 
-let gatoX = 220;
-let gatoY = canvas.height / 2;
-let comidaX = 460;
-let comidaY = 460;
+//Definir variables inicializadas en 0
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+
+//Definir constantes
 const ALTO_GATO= 50;
 const ANCHO_GATO=20;
 const ALTO_COMIDA=40;
 const ANCHO_COMIDA=40;
 
-
+//Iniciar juego asignando posiciones requeridas
 function iniciarJuego() {
-   graficarGato();
-   graficarComida();
-}
+  // Posicionar al gato centrado en el canvas (500x500)
+  gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
+  gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
 
-function graficarGato(){
-     ctx.fillStyle = "#dfa2f2";
-     ctx.fillRect(gatoX,gatoY, ALTO_GATO, ANCHO_GATO);
-}
+  // Posicionar la comida en la esquina inferior derecha
+  comidaX = canvas.width - ANCHO_COMIDA;
+  comidaY = canvas.height - ALTO_COMIDA;
 
+  // Dibujar elementos
+  graficarGato();
+  graficarComida();
+}
+   //Modificado para usar graficarRectangulo
+function graficarGato() {
+  graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "#dfa2f2");
+}
+ //Modificado para usar graficarRectangulo
 function graficarComida(){
-   ctx.fillStyle="#a2f2ee"
-   ctx.fillRect(comidaX,comidaY,ALTO_COMIDA,ANCHO_COMIDA);
+   graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "#a2f2ee");
 }
+
+//Función para graficar cualquier rectángulo
+function graficarRectangulo(x, y, ancho, alto, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, ancho, alto);
+}
+
+
+
